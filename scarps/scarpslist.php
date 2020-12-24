4cb1e7703f5bf7fb768041f6ff6daef5c488f13e
miquelalumnes <68971516+miquelalumnes@users.noreply.github.com>
2020-12-24 12:30:29 +0100

    Add files via upload

---
+++ scarps/scarpslist.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="">
+   <?php include ('../header.php');?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title></title>
+</head>
+
+<body>
+    <h1  align="center">Escales i Arpegis</h1>
+    <div class="container"  align="center">
+        <div class="row">
+
+            <div class="col-6"><a href="piano1/scarps-piano1.php" class="btn btn-info" role="button">Piano grau 1</a></div>
+
+            
+            <div class="col-6"><a href="violin1/scarps-violin1.php" class="btn btn-primary disabled" role="button">Violi grau 1</a></div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-6"><a href="piano2/scarps-piano2.php" class="btn btn-info" role="button">Piano grau 2</a></div>
+            <div class="col-6"><a href="#" class="btn btn-primary disabled" role="button">Violi grau 2</a></div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-6"><a href="#" class="btn btn-info disabled" role="button">Piano grau 3</a></div>
+            <div class="col-6"><a href="#" class="btn btn-primary disabled" role="button">Violi grau 3</a></div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-6"><a href="#" class="btn btn-info disabled" role="button">Piano grau 4</a></div>
+            <div class="col-6"><a href="#" class="btn btn-primary" role="button">Violi grau 4</a></div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-6"><a href="#" class="btn btn-info disabled" role="button">Piano grau 5</a></div>
+            <div class="col-6"><a href="#" class="btn btn-primary disabled" role="button">Violi grau 5</a></div>
+
+        </div>
+        <a href="../practice.php"><h4>Torna enrere</h4></a>
+    </div>
+</body>
+
+</html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../css/bootstrap.min.css">
