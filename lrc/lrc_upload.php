d06fbbf90ccda9e0701c1eef92e9fe1bd2456220
miquelalumnes <68971516+miquelalumnes@users.noreply.github.com>
2020-12-24 12:29:33 +0100

    Add files via upload

---
+++ lrc/lrc_upload.php
@@ -0,0 +1,18 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
+$target_dir = "";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
