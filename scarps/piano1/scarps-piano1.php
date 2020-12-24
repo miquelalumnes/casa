8a15ae8b4af8f61a54b84cdb24063edbd410a78e
miquelalumnes <68971516+miquelalumnes@users.noreply.github.com>
2020-12-24 12:32:17 +0100

    Add files via upload

---
+++ scarps/piano1/scarps-piano1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <title>Piano grau 1</title>
+</head>
+
+<body>
+    <div align="center">
+        <h1>Piano Grau 1</h1>
+
+        <video width="70%" height="70%" controls autoplay onended="videoEnded()" id='video'>
+            <source src="<?php $random = rand(1,23); echo $random; ?>.mp4"> </video> <br>
+
+        <button onClick="slower()">Més lent</button>
+        <button onClick="normal()">Normal</button>
+        <br>
+        <a href="../scarpslist.php">
+            <h2>Torna Enrere</h2>
+        </a>
+    </div>
+
+    <script>
+        var video = document.getElementById('video');
+
+        function slower() {
+            video.playbackRate = 0.5;
+        }
+
+        function normal() {
+            video.playbackRate = 1;
+
+        }
+
+        function videoEnded() {
+            location.reload();
+
+
+        }
+
+    </script>
+
+</body>
+
+</html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
